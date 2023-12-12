9a0b1c31cf117df2c0a54694943e4be80ae35840
Ukaypromise <promiseukaimo@gmail.com>
2023-12-12 21:30:40 +0100

    Load fonts in RootLayout

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,33 @@
 import { Slot, Stack } from "expo-router";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+
+
+import {
+    AmaticSC_700Bold,
+    AmaticSC_400Regular,
+} from "@expo-google-fonts/amatic-sc";
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+    const [fontsLoaded, fontError] = useFonts({
+        Inter: Inter_900Black,
+        Amatic: AmaticSC_400Regular,
+        AmaticBold: AmaticSC_700Bold,
+    });
+
+    useEffect(() => {
+        if (fontsLoaded || fontError) {
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
   return (
     <Stack screenOptions={{}}>
       <Stack.Screen name="index" options={{ title: "Devember" }} />
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,36 +7,12 @@ import {
   ActivityIndicator,
 } from "react-native";
 import DayListItem from "../../src/components/core/DayListItem";
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import {
-  AmaticSC_700Bold,
-  AmaticSC_400Regular,
-} from "@expo-google-fonts/amatic-sc";
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-
-// Keep the splash screen visible while we fetch resources
-SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 // const days = [...Array(24).keys()].map(index => index + 1);
 
 export default function HomeScreen() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
+  
 
   return (
     <View style={styles.container}>
